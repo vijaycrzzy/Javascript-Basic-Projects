e5c6677b214c35849622a6a1b32b55d3ce549c09
vijaycrzzy <101950044+vijaycrzzy@users.noreply.github.com>
2023-01-14 11:51:02 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,26 @@
+
+
+function p1()
+{
+    document.getElementById("p1").innerHTML="Quide repudiandae delectus ab ullam mollitia iusto reprehenderit eaque accusamus sed doloribus animi quasi necessitatibus pariatur eum amet ipsam optio maxime? Vel maxime nihil cumque perferendis. Inventore fugit nihil laboriosam magnam nesciunt esse similique architecto corrupti."
+}
+function p2()
+{
+    document.getElementById("p2").innerHTML="epudiandae delectus ab ullam mollitia iusto reprehenderit eaque accusamus sed doloribus animi quasi necessitatibus pariatur eum amet ipsam optio maxime? Vel maxime nihil cumque perferendis. Inventore fugit nihil laboriosam magnam nesciunt esse similique architecto corrupti."
+}
+function p3()
+{
+    document.getElementById("p3").innerHTML="Quide repudiandae delectus ab ullam mollitia iusto reprehenderit eaque accusamus sed doloribus animi quasi necessitatibus pariatur eum amet ipsam optio maxime? Vel maxime nihil cumque perferendis. Inventore fugit nihil laboriosam "
+}
+function p1r()
+{
+    document.getElementById("p1").innerHTML="";
+}
+function p2r()
+{
+    document.getElementById("p2").innerHTML="";
+}
+function p3r()
+{
+    document.getElementById("p3").innerHTML="";
+}
